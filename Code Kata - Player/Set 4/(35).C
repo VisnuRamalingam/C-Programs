ac15a969e7d6e4394ee85c0df634a53296dd3471
VisnuRamalingam <46813995+VisnuRamalingam@users.noreply.github.com>
2019-03-26 07:10:23 +0530

    Add files via upload

---
+++ Code Kata - Player/Set 4/(35).C
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+char s[100000];
+ int i,j,count=0;
+ scanf("%[^\n]s",s);
+ for(i=0;s[i]!='\0';i++)
+ {count=0;
+    for(j=i+1;s[j]!='\0';j++)
+    {
+        if(s[i]!='#'&&s[i]!='#')
+        {
+        if(s[i]==s[j])
+        {
+           count++;
+           s[j]='#';
+            
+        }
+    }}
+     
+     if(count==0&&s[i]!='#')
+    {
+     printf("%c ",s[i]);
+    }
+ }
+return 0; 
+}
---
+++ Code Kata - Player/Set 8/(76).C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+
+{
+    int M,s1[10],i,d=0,r,j,t=0;
+    scanf("%d",&M);
+    for(i=0;i<M;i++)
+    {
+        scanf("%d",&s1[i]);
+    }
+    for(i=0;i<M;i++)
+    {
+        for(j=i+1;j<M;j++)
+        {
+            if(s1[i]>s1[j])
+            {
+             d=s1[i]-s1[j];
+            }
+            else
+            {
+                d=s1[j]-s1[i];
+            }
+            if(d>t)
+            {
+                t=d;
+            }
+        }
+    }
+        printf("%d",t);
+return 0;
+}
